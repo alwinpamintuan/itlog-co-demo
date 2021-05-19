8053ebc3c310b4353d1451686ab4d59cec4ff666
alwinpamintuan <jrpamintuan1@up.edu.ph>
2021-05-20 01:26:54 +0800

    feat: navbar

---
+++ src/components/molecules/brand/Brand.js
@@ -0,0 +1,13 @@
+import Header from '../../atoms/header/Header'
+import './styles.css'
+
+function Brand(props){
+    return(
+        <div className="row">
+            <img src={props.logo} alt={props.children} className={props.logoClass}></img>
+            <Header className={props.headerClass}>{props.children}</Header>
+        </div>
+    )
+}
+
+export default Brand
---
+++ src/components/organisms/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Brand from '../molecules/brand/Brand'
+import LinkGroup from '../molecules/linkgroup/LinkGroup'
+import './styles.css'
+
+function NavBar(props){
+    return(
+        <div className={props.className}>
+            <Brand
+                headerClass={props.content.headerClass}
+                logo={props.content.logo}
+                logoClass={props.content.logoClass}
+            >
+                    {props.content.brand}
+            </Brand>
+            <LinkGroup
+                className="flex-end"
+            >
+                {props.content.links}
+            </LinkGroup>
+        </div>
+    )
+}
+
+export default NavBar;
